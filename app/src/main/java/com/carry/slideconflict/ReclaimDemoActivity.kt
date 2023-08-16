7d449c44f7913df95ffefee8d142ae3204115958
SunnyDay <1246771571@qq.com>
2023-08-16 21:39:08 +0800

    Initial commit

---
+++ app/src/main/java/com/carry/slideconflict/ReclaimDemoActivity.kt
@@ -0,0 +1,14 @@
+package com.carry.slideconflict
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Create by SunnyDay /08/16 21:30:56
+ */
+class ReclaimDemoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reclaim_demo)
+    }
+}
